eb6f604afa01d8ccaa39eca712bd8f61a75465de
Kristina Papić <papic.kris@gmail.com>
2018-05-20 14:26:14 +0200

    updated model for core

---
+++ poslovna_banka/src/main/java/poslovna_banka/PoslovnaBankaApplication.java
@@ -0,0 +1,12 @@
+package poslovna_banka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+}
