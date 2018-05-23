424389ff4000271c65a383196786fec352030fe6
DESKTOP-RI8MMLC\Dejan <dejan8e@gmail.com>
2018-05-23 13:05:38 +0200

    test commit

--- poslovna_banka/src/main/java/poslovna_banka/repository/ExchangeRateRepository.java
+++ poslovna_banka/src/main/java/poslovna_banka/repository/ExchangeRateRepository.java
@@ -1,5 +1,13 @@
 package poslovna_banka.repository;
 
-public class ExchangeRateRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import poslovna_banka.model.ExchangeRate;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate,Long> {
+	
+	//test
 
 }
