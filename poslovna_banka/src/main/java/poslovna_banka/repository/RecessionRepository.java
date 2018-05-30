69cf55e6f7d9ed166ff19705d7da11a70bee51cf
Sinisa <sikann95@gmail.com>
2018-05-30 19:09:16 +0200

    recession add on backend

---
+++ poslovna_banka/src/main/java/poslovna_banka/repository/RecessionRepository.java
@@ -0,0 +1,10 @@
+package poslovna_banka.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import poslovna_banka.model.Recession;
+
+
+public interface RecessionRepository extends CrudRepository<Recession,Long> {
+
+}
