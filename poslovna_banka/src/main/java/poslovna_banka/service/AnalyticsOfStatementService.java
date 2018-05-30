a0925da64b97dff29f79c4f3f0f9c9c92e507dd5
Probook450G2\Milan <kovacevicmilica544@gmail.com>
2018-05-30 09:30:20 +0200

    no message

--- poslovna_banka/src/main/java/poslovna_banka/service/AnalyticsOfStatementService.java
+++ poslovna_banka/src/main/java/poslovna_banka/service/AnalyticsOfStatementService.java
@@ -10,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import poslovna_banka.model.AnalyticOfStatement;
+import poslovna_banka.model.BankAccount;
+import poslovna_banka.model.LegalEntity;
 import poslovna_banka.repository.AnalyticOfStatementRepository;
 import poslovna_banka.repository.BankAccountRepository;
+import poslovna_banka.repository.LegalEntityRepository;
 
 @Service
 public class AnalyticsOfStatementService {
@@ -22,6 +25,9 @@ public class AnalyticsOfStatementService {
 	@Autowired
 	private BankAccountRepository bankAccountRepository;
 
+	@Autowired
+	private LegalEntityRepository legalEntityRepository;
+
 	public AnalyticOfStatement getAnalyticsOfStatements(File file) throws JAXBException {
 
 		JAXBContext jaxbContext = JAXBContext.newInstance(AnalyticOfStatement.class);
@@ -31,7 +37,17 @@ public class AnalyticsOfStatementService {
 		analyticRepository.save(a);
 
 		if (a.getType().equals("Nalog za isplatu")) {
-
+			BankAccount debtorAccount = bankAccountRepository.findOne(a.getDebtorAccount().getId());
+			// DailyAccountBalance dailyAccountBalance =
+			// dailyAccountBalanceService.findByAccountNumberAndDate(debtorAccount,
+			// a.getCurrencyDate());
+			// dailyAccountBalance.setTrafficToTheBurden(dailyAccountBalance.getTrafficToTheBurden()
+			// + a.getSum());
+			// dailyAccountBalance.setNewState(dailyAccountBalance.getPreviousState()+
+			// dailyAccountBalance.getTrafficToBenefit() -
+			// dailyAccountBalance.getTrafficToTheBurden());
+			// dailyAccountBalance.getAnalyticsOfStatements().add(a);
+			// dailyAccountBalanceService.save(dailyAccountBalance);
 		}
 
 		return a;
