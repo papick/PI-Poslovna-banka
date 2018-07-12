708e03da3b9e9a13305d7d1c2e8a4558faf23cc6
Probook450G2\Milan <kovacevicmilica544@gmail.com>
2018-07-12 10:00:22 +0200

    ..

--- pi-front/src/app/app.module.ts
+++ pi-front/src/app/app.module.ts
@@ -1,52 +1,47 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
-import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {AppRoutingModule} from "./app-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ReactiveFormsModule} from '@angular/forms';
-import {LogInComponent} from "./log-in.component/log-in.component";
-import {APP_BASE_HREF} from "@angular/common";
-import {LogInService} from "../service/logInService";
+import {AppComponent} from "./app.component";
 import {BankComponent} from "./bank/bank.component";
+import {LogInComponent} from "./log-in.component/log-in.component";
 import {PaymentCheckComponent} from "./payment-check/payment-check.component";
-import {CountryService} from "../service/countryService";
 import {CountriesComponent} from "./countries/countries.component";
 import {AddCountryComponent} from "./addCountry/addCountry.component";
-import {CityService} from "../service/cityService";
-import {CityComponent} from './city/city.component';
+import {CityComponent} from "./city/city.component";
+import {CurrenciesComponent} from "./currencies/currencies.component";
 import {AddCityComponent} from "./addCity/addCity.component";
 import {ActivitiesComponent} from "./activities/activities.component";
-import {ActivityService} from "../service/activityService";
 import {AddActivitiesComponent} from "./addActivities/addActivities.component";
-import {BankAccountsComponent} from './bank-accounts/bank-accounts.component';
-import {ClientService} from "../service/clientService";
+import {BankAccountsComponent} from "./bank-accounts/bank-accounts.component";
 import {PaymentOrderComponent} from "./payment-order/payment-order.component";
-import {AddLegalsComponent} from './add-legals/add-legals.component';
-import { AddBankAccountIndividualComponent } from './add-bank-account-individual/add-bank-account-individual.component';
-import {BankAccountService} from "../service/bankAccountService";
-import {CurrencyService} from "../service/currencyService";
-import {AnalyticsOfStatementService} from '../service/AnalyticsOfStatementService';
-import {CurrenciesComponent} from "./currencies/currencies.component";
+import {AddLegalsComponent} from "./add-legals/add-legals.component";
+import {AddBankAccountIndividualComponent} from "./add-bank-account-individual/add-bank-account-individual.component";
 import {CurrencyFormComponent} from "./currency-form/currency-form.component";
 import {ComboZoomCountriesComponent} from "./combo-zoom-countries/combo-zoom-countries.component";
-import { PayCheckComponent } from './pay-check/pay-check.component';
-import { TransferCheckComponent } from './transfer-check/transfer-check.component';
-import { AddLegalAccountComponent } from './add-legal-account/add-legal-account.component';
-import { AddIndividualComponent } from './add-individual/add-individual.component';
-import { LegalsComponent } from './legals/legals.component';
-import { ClientsComponent } from './clients/clients.component';
-import { IndividualsComponent } from './individuals/individuals.component';
+import {PayCheckComponent} from "./pay-check/pay-check.component";
+import {TransferCheckComponent} from "./transfer-check/transfer-check.component";
+import {AddLegalAccountComponent} from "./add-legal-account/add-legal-account.component";
+import {AddIndividualComponent} from "./add-individual/add-individual.component";
+import {LegalsComponent} from "./legals/legals.component";
+import {ClientsComponent} from './clients/clients.component';
+import {IndividualsComponent} from "./individuals/individuals.component";
+import {AnalyticsOfStatementService} from "../service/AnalyticsOfStatementService";
+import {CurrencyService} from "../service/currencyService";
+import {ClientService} from '../service/clientService';
+import {BankAccountService} from "../service/bankAccountService";
+import {ActivityService} from "../service/activityService";
+import {CityService} from "../service/cityService";
+import {CountryService} from "../service/countryService";
+import {LogInService} from "../service/logInService";
+import {APP_BASE_HREF} from '@angular/common';
 import {RecessionService} from "../service/recessionService";
-
-
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import {DropdownModule} from 'primeng/dropdown';
-import {FormsModule} from '@angular/forms';
-import {SharedModule} from 'primeng/primeng';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ButtonModule} from "primeng/button";
+import {DialogModule} from "primeng/dialog";
+import {SharedModule} from 'primeng/components/common/shared';
+import {DropdownModule} from "primeng/primeng";
 
 
 @NgModule({
